f902a1c2a3b40a7cee8a0d75c5f0cf98f7243cec
OrSteiner <39240606+OrSteiner@users.noreply.github.com>
2019-05-07 12:48:01 +0300

    init

---
+++ hw2/models.py
@@ -0,0 +1,167 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from .blocks import Block, Linear, ReLU, Sigmoid, Dropout, Sequential
+
+
+class MLP(Block):
+    """
+    A simple multilayer perceptron model based on our custom Blocks.
+    Architecture is (with ReLU activation):
+
+        FC(in, h1) -> ReLU -> FC(h1,h2) -> ReLU -> ... -> FC(hn, num_classes)
+
+    Where FC is a fully-connected layer and h1,...,hn are the hidden layer
+    dimensions.
+    If dropout is used, a dropout layer is added after every activation
+    function.
+    """
+    def __init__(self, in_features, num_classes, hidden_features=(),
+                 activation='relu', dropout=0, **kw):
+        super().__init__()
+        """
+        Create an MLP model Block.
+        :param in_features: Number of features of the input of the first layer.
+        :param num_classes: Number of features of the output of the last layer.
+        :param hidden_features: A sequence of hidden layer dimensions.
+        :param activation: Either 'relu' or 'sigmoid', specifying which 
+        activation function to use between linear layers.
+        :param: Dropout probability. Zero means no dropout.
+        """
+        blocks = []
+
+        # TODO: Build the MLP architecture as described.
+        # ====== YOUR CODE: ======
+        for dim_out in hidden_features:
+            blocks.append(Linear(in_features, dim_out))
+            if activation == 'relu':
+                blocks.append(ReLU())
+            if activation == 'sigmoid':
+                blocks.append(Sigmoid())
+            in_features = dim_out
+            if dropout > 0:
+                blocks.append(Dropout(dropout))
+
+        blocks.append(Linear(in_features, num_classes))
+        # blocks.append(ReLU())
+        # ========================
+
+        self.sequence = Sequential(*blocks)
+
+    def forward(self, x, **kw):
+        return self.sequence(x, **kw)
+
+    def backward(self, dout):
+        return self.sequence.backward(dout)
+
+    def params(self):
+        return self.sequence.params()
+
+    def train(self, training_mode=True):
+        self.sequence.train(training_mode)
+
+    def __repr__(self):
+        return f'MLP, {self.sequence}'
+
+
+class ConvClassifier(nn.Module):
+    """
+    A convolutional classifier model based on PyTorch nn.Modules.
+
+    The architecture is:
+    [(Conv -> ReLU)*P -> MaxPool]*(N/P) -> (Linear -> ReLU)*M -> Linear
+    """
+    def __init__(self, in_size, out_classes, filters, pool_every, hidden_dims):
+        """
+        :param in_size: Size of input images, e.g. (C,H,W).
+        :param out_classes: Number of classes to output in the final layer.
+        :param filters: A list of of length N containing the number of
+            filters in each conv layer.
+        :param pool_every: P, the number of conv layers before each max-pool.
+        :param hidden_dims: List of of length M containing hidden dimensions of
+            each Linear layer (not including the output layer).
+        """
+        super().__init__()
+        self.in_size = in_size
+        self.out_classes = out_classes
+        self.filters = filters
+        self.pool_every = pool_every
+        self.hidden_dims = hidden_dims
+
+        self.feature_extractor = self._make_feature_extractor()
+        self.classifier = self._make_classifier()
+
+    def _make_feature_extractor(self):
+        in_channels, in_h, in_w, = tuple(self.in_size)
+
+        layers = []
+        # TODO: Create the feature extractor part of the model:
+        # [(Conv -> ReLU)*P -> MaxPool]*(N/P)
+        # Use only dimension-preserving 3x3 convolutions. Apply 2x2 Max
+        # Pooling to reduce dimensions.
+        # ====== YOUR CODE: ======
+        N = self.filters
+        P = self.pool_every
+        count = 0
+        for out_channels in self.filters:
+            layers.append(nn.Conv2d(in_channels, out_channels, 3, padding=1))
+            layers.append(nn.ReLU())
+            in_channels = out_channels
+            count += 1
+            if count == P:
+                layers.append(nn.MaxPool2d(2))
+                in_h //= 2
+                in_w //= 2
+                count = 0
+        self.in_h = in_h
+        self.in_w = in_w
+        # ========================
+        seq = nn.Sequential(*layers)
+        return seq
+
+    def _make_classifier(self):
+        in_channels, in_h, in_w, = tuple(self.in_size)
+
+        layers = []
+        # TODO: Create the classifier part of the model:
+        # (Linear -> ReLU)*M -> Linear
+        # You'll need to calculate the number of features first.
+        # The last Linear layer should have an output dimension of out_classes.
+        # ====== YOUR CODE: ======
+        in_dim = self.filters[-1] * self.in_w * self.in_h
+        for hidden in self.hidden_dims:
+            layers.append(nn.Linear(in_dim, hidden))
+            layers.append(nn.ReLU())
+            in_dim = hidden
+        layers.append(nn.Linear(in_dim, self.out_classes))
+        # ========================
+        seq = nn.Sequential(*layers)
+        return seq
+
+    def forward(self, x):
+        # TODO: Implement the forward pass.
+        # Extract features from the input, run the classifier on them and
+        # return class scores.
+        # ====== YOUR CODE: ======
+        # print(x.size())
+        out = self.feature_extractor(x)
+        # print(out.size())
+        # print(out.flatten().size())
+        out = self.classifier(out.flatten())
+        # ========================
+        return out
+
+
+class YourCodeNet(ConvClassifier):
+    def __init__(self, in_size, out_classes, filters, pool_every, hidden_dims):
+        super().__init__(in_size, out_classes, filters, pool_every, hidden_dims)
+
+    # TODO: Change whatever you want about the ConvClassifier to try to
+    # improve it's results on CIFAR-10.
+    # For example, add batchnorm, dropout, skip connections, change conv
+    # filter sizes etc.
+    # ====== YOUR CODE: ======
+    # raise NotImplementedError()
+    # ========================
+
